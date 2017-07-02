8d2c27252bb8742b99cb0f466ad8b89a1c4b6b5c
liaojian <1600136170@qq.com>
2017-07-02 08:11:06 +0800

    Site updated: 2017-07-02 08:11:05

--- js/me/main.js
+++ js/me/main.js
@@ -25,12 +25,10 @@
             $gallery = $('.gallery');
 
         // Disable animations/transitions until the page has loaded.
-        $body.addClass('is-loading');
-
         $window.on('load', function () {
             window.setTimeout(function () {
                 $body.removeClass('is-loading');
-            }, 0);
+            }, 1);
         });
 
         // Touch mode.
